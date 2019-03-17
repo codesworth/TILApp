7eadb79d3e7393f009c57765fca6ef2504315674
Codesworth <codezworth@gmail.com>
2019-03-17 23:08:07 +0000

    Embeded templates

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -3,14 +3,17 @@ import Vapor
 
 struct WebsiteController:RouteCollection {
     
+    typealias WebView = Future<View>
+    
     func boot(router: Router) throws {
         router.get(use: indexHandler)
         router.get("acronyms",Acronyms.parameter, use: acronymsHnadler)
+        router.get("users", User.parameter,use:userHandler)
     }
     
     func indexHandler(_ req:Request) throws -> Future<View>{
         return Acronyms.query(on: req).all().flatMap(to: View.self){ acronyms in
-            let context = IndexContext(title: "HomePage", acronyms:acronyms.isEmpty ? nil : acronyms)
+            let context = IndexContext(title: "HomePage", acronyms: acronyms)
             return try req.view().render("index",context)
         }
         
@@ -24,6 +27,13 @@ struct WebsiteController:RouteCollection {
                let context = AcronymContext(title: acronym.long, Acronym: acronym, user: user)
                 return try req.view().render("acronym", context)
             }
+        }
+    }
+    
+    func userHandler(_ req:Request)throws ->WebView{
+        return try req.parameters.next(User.self).flatMap(to: View.self){ user in
+            let ctx = UserContxt(title: user.name, user: user, acronyms: try user.acronyms.query(on: req).all())
+            return try req.view().render("user", ctx)
         }
     }
 }
@@ -41,4 +51,11 @@ struct AcronymContext:Encodable {
     let title:String
     let Acronym:Acronyms
     let user:User
+}
+
+struct UserContxt:Encodable{
+    
+    let title:String
+    let user:User
+    let acronyms:Future<[Acronyms]>
 }
